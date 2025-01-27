eaffb6a93523d713c64ccb07493dd6c144e1c263
Manuel Toleran <mtoleran@my.centennialcollege.ca>
2025-01-26 22:50:15 -0500

    Lab 1 Question 3

---
+++ COMP367001_Lab1-Toleran_Q3/COMP367001_Lab1-Toleran_Q3/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace COMP367001_Lab1_Toleran_Q3.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
